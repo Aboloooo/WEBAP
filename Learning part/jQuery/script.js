0301142a6f34b80f93eef75d13c1187d89a252ad
abolo <abolfazle0915501@gmail.com>
2025-10-29 21:29:19 +0100

    df

--- Learning part/jQuery/script.js
+++ Learning part/jQuery/script.js
@@ -1,3 +1,8 @@
-readyDocument(){
+/* readyDocument(){
     // as soon as loading page colmplete this function will start working
-}
+} */
+
+$(document).ready(function () {
+  console.log("file loaded completly!");
+  $("h1").text("changed");
+});
