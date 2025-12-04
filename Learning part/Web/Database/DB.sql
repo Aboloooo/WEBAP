b74686dc9e34e5a5a4530a0ab125eb856d3e5515
abolo <abolfazle0915501@gmail.com>
2025-12-04 11:43:26 +0100

    erg

--- Learning part/Web/Database/DB.sql
+++ Learning part/Web/Database/DB.sql
@@ -4,16 +4,18 @@ USE PIF2025_2026;
 /* adding forign key and some table left */
 
 CREATE TABLE User(
-    Username VARCHAR(50) PRIMARY KEY,
+    Username VARCHAR(255) PRIMARY KEY,
     First_name CHAR(50) NOT NULL,
     Last_name CHAR(50) NOT NULL,
-    Role VARCHAR(50) NOT NULL
+    Role VARCHAR(50) NOT NULL,
 );
 CREATE TABLE Station(
     Station_id int PRIMARY KEY AUTO_INCREMENT,
     Serial_number VARCHAR(255) NOT NULL,
     Name VARCHAR(50),
-    Description CHAR
+    Description CHAR,
+    Owner VARCHAR(255),
+    FOREIGN KEY (Owner) REFERENCES User(Username) 
 );
 CREATE TABLE Measurement(
     Measurement_id int PRIMARY KEY AUTO_INCREMENT,
@@ -21,13 +23,22 @@ CREATE TABLE Measurement(
     Humidity VARCHAR(255),
     Air_pressure VARCHAR(255),
     Light_intensity VARCHAR(255),
-    Air_quality VARCHAR(255)
+    Air_quality VARCHAR(255),
+    Station_id VARCHAR(255),
+    FOREIGN KEY (Station_id) REFERENCES Station(Station_id) 
 );
 CREATE TABLE Collection(
     Collection_id int PRIMARY KEY AUTO_INCREMENT,
     Name VARCHAR(50) NOT NULL,
-    Description CHAR
+    Description CHAR,
+    Measurement_id VARCHAR(255),
+    FOREIGN KEY (Measurement_id) REFERENCES Measurement(Measurement_id) 
 );
-CREATE TABLE CollectionMeasurement();
+CREATE TABLE CollectionMeasurement(
+    Username VARCHAR(255),
+    Collection_id int,
+    FOREIGN KEY (Username) REFERENCES User(Username),
+    FOREIGN KEY (Collection_id) REFERENCES Collection(Collection_id) 
+);
 CREATE TABLE Friendship();
 CREATE TABLE CollectionShare();
